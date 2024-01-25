060572f4206c42cda0d71b33ac0df320ecddb27f
Nicolas <ni.berlaud@gmail.com>
2024-01-25 17:02:49 +0100

    ✨ Add PHP Header

--- index.php
+++ index.php
@@ -4,3 +4,9 @@
 <?php
     include_once __DIR__ ."/pages/head.inc.php";
 ?>
+<body>
+<?php
+    include_once __DIR__ ."/pages/header.inc.php";
+?>
+</body>
+</html>
---
+++ pages/header.inc.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="header-logo-text-container">
+        <img src="./asset/images.png" alt="logo gmail" class="header-logo">
+        <div class="header-logo-text">Gmail</div>
+    </div>
+    <a href="#signUpForm">
+        <div class="btn btn__square btn--white text--uppercase">pour les pros</div>
+    </a>
+    <a href="./connexion.html">
+        <div class="btn btn__square btn--white text--uppercase">connexion</div>
+    </a>
+    <a href="#signUpForm">
+        <div class="btn btn__square btn--red text--uppercase">créer un compte</div>
+    </a>
+</header>
