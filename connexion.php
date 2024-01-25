16770f02fa0201f497edbd7f5e7f614325a0dc76
Nicolas <ni.berlaud@gmail.com>
2024-01-25 17:13:55 +0100

    ✨ Add PHP Connexion Header

--- connexion.php
+++ connexion.php
@@ -5,5 +5,8 @@
     include_once __DIR__ ."/pages/connexion/head.inc.php";
 ?>
 <body>
+<?php
+    include_once __DIR__ ."/pages/connexion/header.inc.php";
+?>
 </body>
 </html>
---
+++ pages/connexion/header.inc.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="header-logo-text-container">
+        <img src="./asset/images.png" alt="logo gmail" class="header-logo">
+        <div class="header-logo-text">Gmail</div>
+    </div>
+    <a href="./index.html#welcomePage">
+        <div class="btn btn__square btn--white text--uppercase">pour les pros</div>
+    </a>
+    <a href="./connexion.html">
+        <div class="btn btn__square btn--red text--uppercase">connexion</div>
+    </a>
+    <a href="./index.html#signUpForm">
+        <div class="btn btn__square btn--white text--uppercase">créer un compte</div>
+    </a>
+</header>
