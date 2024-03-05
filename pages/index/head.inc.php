7e1badcaf630e2a5fd1999c2f92f7bcfec0bdd5f
Nicolas <ni.berlaud@gmail.com>
2024-03-05 20:37:18 +0100

    ✨ feature: Change style path

--- pages/index/head.inc.php
+++ pages/index/head.inc.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/index.css">
     <title>FakeGmail</title>
     <script src="./script/script.js"></script>
 </head>
