25f57a7164d3d52739ce8578b65b2a20b5724205
Nicolas <ni.berlaud@gmail.com>
2024-03-05 09:45:36 +0100

    ✨ feature: Add style and script
    Change the style location and add the script to get notifications

--- pages/connexion/head.inc.php
+++ pages/connexion/head.inc.php
@@ -2,6 +2,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/connexion.css">
     <title>FakeGmail</title>
+    <script src="./script/script.js"></script>
 </head>
