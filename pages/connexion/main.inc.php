5c8e62eee9c369a07d645adece288ed3b9482ab1
Nicolas <ni.berlaud@gmail.com>
2024-03-05 20:52:38 +0100

    ✨ feature: Add PHP & show if account created
    Big commit ...

--- pages/connexion/main.inc.php
+++ pages/connexion/main.inc.php
@@ -1,8 +1,21 @@
-<main>
-    <h3 class="inbox-container">Bienvenue dans votre espace null</h3>
+<main class="vertical__full">
+    <?php
+    if ($_SESSION['user_created'] == 'true')
+    {
+        echo "<script>showNotification('Votre compte a bien été créé, veuillez vous connectez', 'success');</script>";
+        $_SESSION['user_created'] = 'false';
+    }
+    ?>
     <div id="logInForm" class="form">
+        <?php
+        use pages\controller\UserManager;
+
+        include_once './pages/controller/UserManager.php';
+
+        UserManager::logIn();
+        ?>
         <div class="form__title">Connectez-vous à votre compte</div>
-        <form action="./connexion.html" method="post">
+        <form action="<?php print htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <ul class="form__content">
                 <li class="form__question">
                     <label class="form__label" for="mail">Mail : *</label>
