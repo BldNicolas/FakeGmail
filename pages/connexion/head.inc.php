6ce4f643dd5e8595df3c2df65af34d24548b1f61
Nicolas <ni.berlaud@gmail.com>
2024-03-07 16:21:49 +0100

    ✨ feature: Add Favicon

--- pages/connexion/head.inc.php
+++ pages/connexion/head.inc.php
@@ -3,6 +3,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <link rel="stylesheet" href="./css/connexion.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="asset/icon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="asset/icon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="asset/icon/favicon-16x16.png">
+    <link rel="manifest" href="asset/icon/site.webmanifest">
     <title>FakeGmail</title>
     <script src="./script/script.js"></script>
     <script src="https://kit.fontawesome.com/234a90ccd9.js" crossorigin="anonymous"></script>
