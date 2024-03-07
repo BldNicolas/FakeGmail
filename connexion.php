d16dd91af281b8d2e7e1ea7a20e9defa4e2d557a
Nicolas <ni.berlaud@gmail.com>
2024-03-07 16:45:10 +0100

    🔨 refactor: Rename connexion to connection

--- connexion.php
+++ connexion.php
@@ -5,13 +5,13 @@
 <html lang="fr">
 
 <?php
-    include_once __DIR__ ."/pages/connexion/head.inc.php";
+    include_once __DIR__ . "/pages/connection/head.inc.php";
 ?>
 <body>
 <?php
-    include_once __DIR__ ."/pages/connexion/header.inc.php";
-    include_once __DIR__ ."/pages/connexion/main.inc.php";
-    include_once __DIR__ ."/pages/connexion/footer.inc.php";
+    include_once __DIR__ . "/pages/connection/header.inc.php";
+    include_once __DIR__ . "/pages/connection/main.inc.php";
+    include_once __DIR__ . "/pages/connection/footer.inc.php";
 ?>
 </body>
 </html>
